53f2b4c336066d59b2ed5bd4980b7cb1c56e0116
Mamatha Suresh <Mamatha Suresh@localhost>
2026-05-24 23:11:16 +0530

    Add Python30excs folder

---
+++ Python30excs/Day2.py
@@ -0,0 +1,61 @@
+#Day 2: 30 Days of python programming
+first_name= input("Enter your first name: ")
+last_name= input("Enter your last name: ")
+full_name=first_name + ''+ last_name
+country='India'
+city='Bangalore'
+age=25
+year=2024
+is_married=False
+is_true=True
+is_light_on=True
+#first_name, last_name, country, age, is_married = 'Mamatha', 'NS', 'India', 25, True
+print(full_name)      
+print(country)
+print(city)
+print(age)
+print(year)
+print(is_married)
+print(is_true)
+print(is_light_on)
+
+# checking data types
+
+print(type(first_name))
+print(type(last_name))
+print(type(country))
+print(type(age))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+
+#finding length
+
+print(len(first_name))
+print(len(last_name))
+
+num_one=5
+num_two=4
+total=num_one+num_two
+print(total)
+subtraction=num_one-num_two
+print(subtraction)
+multiplication=num_one*num_two
+print(multiplication)
+division=num_one/num_two
+print(division)
+modulus=num_one%num_two
+print(modulus)
+floor_division=num_one//num_two 
+print(floor_division)
+power=num_one**num_two
+print(power)
+
+#radius of a circle
+radius= int(input("Enter the radius of the circle: "))  
+area_of_circle=3.14*radius**2
+print(area_of_circle)  
+circumference_of_circle=2*3.14*radius
+print(circumference_of_circle)
+print("The area of a circle with radius", radius, "is", area_of_circle)
+
---
+++ Python30excs/python1.py
@@ -0,0 +1,10 @@
+print("Hello World!") 
+
+#Data types#
+print(type(10)) #int
+print(type(10.5)) #float
+print(type("Hello")) #str
+print(type(1+2j)) #complex
+print(type(True)) #bool   
+print(type([1,2,3])) #list
+print(type((1,2,3))) #tuple 
